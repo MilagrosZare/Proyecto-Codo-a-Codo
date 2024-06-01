3aaf5e7662123b3e58098f1fd811d04e0fa0052a
Milagros.Zare <miluurosario13@gmail.com>
2024-06-01 02:17:22 -0300

    Modificaciones en el HTML

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 /* =================== TYPING ANIMATION ====================== */
 var typed = new Typed (".typing",{
-    strings:["","Web Designer", "Web Developer","Graphic Designer","Youtuber",""],
+    strings:["","Full Stack Developer", "Web Applications Developer","UX Designer","Software Engineer",""],
 typeSpeed:100,
 BackSpeed:60,
 loop:true
