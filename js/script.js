1c6894ec2b37473d87d7326aceda9c5b9c47c134
rosana.rojas <rosana.19abril@gmail.com>
2024-06-02 21:58:08 -0300

    Format Document

--- js/script.js
+++ js/script.js
@@ -1,76 +1,62 @@
 
 
 /* =================== TYPING ANIMATION ====================== */
-var typed = new Typed (".typing",{
-    strings:["","Full Stack Developer", "Web Applications Developer","UX Designer","Software Engineer",""],
-typeSpeed:100,
-BackSpeed:60,
-loop:true
+var typed = new Typed(".typing", {
+    strings: ["", "Full Stack Developer", "Web Applications Developer", "UX Designer", "Software Engineer", ""],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
 /* ===== HEADER ===== */
 const nav = document.querySelector(".nav"),
-	navList = nav.querySelectorAll("li"),
-	totalNavList = navList.length,
-	allSection = document.querySelectorAll(".section"),
-	totalSection = allSection.length;
-	for(let i=0; i<totalNavList; i++)
-	{
-		const a = navList[i].querySelector("a");
-		a.addEventListener("click", function()
-		{
-			removeBackSection();
-                for(let j=0; j<totalNavList; j++)
-                {
-                    if(navList[j].querySelector("a").classList.contains("active"))
-                    {
-                        addBackSection(j);
-                        //allSection[j].classList.add("back-section");
-                    }
-                    navList[j].querySelector("a").classList.remove("active");
-                }
-			this.classList.add("active")
-			showSection(this);
-			if(window.innerWidth < 1200)
-			{
-				headerSectionTogglerBtn();
-		    }
-        })
-    }
-function removeBackSection()
-{
-    for(let i=0; i<totalSection; i++)
-         {
-            allSection[i].classList.remove("back-section");
-         }
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        removeBackSection();
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
+                //allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+        if (window.innerWidth < 1200) {
+            headerSectionTogglerBtn();
+        }
+    })
 }
-function addBackSection(num)
-{
+function removeBackSection() {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
+    }
+}
+function addBackSection(num) {
     allSection[num].classList.add("back-section");
 }
-function showSection(element)
-{
-	for(let i=0 ; i<totalSection ; i++)
-	{
-		allSection[i].classList.remove("active");
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
     }
-	const target = element.getAttribute("href").split("#")[1];
-	document.querySelector("#" + target).classList.add("active")
-	
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+
 }
-function updateNav(element)
-{
-    for(let i=0; i<totalNavList; i++)
-    {
+function updateNav(element) {
+    for (let i = 0; i < totalNavList; i++) {
         navList[i].querySelector("a").classList.remove("active");
         const target = element.getAttribute("href").split("#")[1];
-        if(target === navList[i].querySelector("a").getAttribute("href").split("#"))
-        {
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")) {
             navList[i].querySelector("a").classList.add("active");
         }
     }
 }
-document.querySelector(".hire-me").addEventListener("click", function()
-{
+document.querySelector(".hire-me").addEventListener("click", function () {
     const sectionIndex = this.getAttribute("data-section-index");
     showSection(this);
     updateNav(this);
@@ -78,18 +64,15 @@ document.querySelector(".hire-me").addEventListener("click", function()
     addBackSection(sectionIndex);
 })
 const navTogglerBtn = document.querySelector(".ham"),
-header = document.querySelector(".nav");
+    header = document.querySelector(".nav");
 const barras = document.querySelectorAll(".ham span");
 
-navTogglerBtn.addEventListener("click", () =>
-    {
-         headerSectionTogglerBtn()
-    })     
-function headerSectionTogglerBtn()
-{   
+navTogglerBtn.addEventListener("click", () => {
+    headerSectionTogglerBtn()
+})
+function headerSectionTogglerBtn() {
     header.classList.toggle("active");
-    barras.forEach(child =>
-    {
+    barras.forEach(child => {
         child.classList.toggle("animated");
     });
 }
@@ -115,52 +98,52 @@ const campos = {
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
-            case "nombre":
-                validarCampo(expresiones.nombre, e.target, 'nombre');
+        case "nombre":
+            validarCampo(expresiones.nombre, e.target, 'nombre');
             break;
-            case "email":
-                validarCampo(expresiones.email, e.target, 'email');
+        case "email":
+            validarCampo(expresiones.email, e.target, 'email');
             break;
-            case "telefono":
-                validarCampo(expresiones.telefono, e.target, 'telefono');
+        case "telefono":
+            validarCampo(expresiones.telefono, e.target, 'telefono');
             break;
-            case "asunto":
-                validarCampo(expresiones.asunto, e.target, 'asunto');
+        case "asunto":
+            validarCampo(expresiones.asunto, e.target, 'asunto');
             break;
-            
-        }
+
+    }
 }
 
 const validarCampo = (expresion, input, campo) => {
-    if(expresion.test(input.value)){
-            document.getElementById(`campo_${campo}`).classList.remove('form-group-incorrecto');
-            document.getElementById(`campo_${campo}`).classList.add('form-group-correcto');
-            document.querySelector(`#campo_${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-            campos[campo] = true;
+    if (expresion.test(input.value)) {
+        document.getElementById(`campo_${campo}`).classList.remove('form-group-incorrecto');
+        document.getElementById(`campo_${campo}`).classList.add('form-group-correcto');
+        document.querySelector(`#campo_${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
     } else {
-            document.getElementById(`campo_${campo}`).classList.add('form-group-incorrecto');
-            document.getElementById(`campo_${campo}`).classList.remove('form-group-correcto');
-            document.querySelector(`#campo_${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-            campos[campo] = false;
+        document.getElementById(`campo_${campo}`).classList.add('form-group-incorrecto');
+        document.getElementById(`campo_${campo}`).classList.remove('form-group-correcto');
+        document.querySelector(`#campo_${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
     }
 }
 
 
-    //obtener valor select
-    let selectServicios = document.getElementById('servicios')
-    let valueTexto = selectServicios.options[selectServicios.selectedIndex].value
-    //console.log(valueTexto)
+//obtener valor select
+let selectServicios = document.getElementById('servicios')
+let valueTexto = selectServicios.options[selectServicios.selectedIndex].value
+//console.log(valueTexto)
 
-    //obtener valor checkbox
-    let checkboxes = {}
-    document.querySelectorAll('[type="checkbox"]').forEach(item => {
-        if (item.checked === true){
-            checkboxes[item.value] = true
-        } else if (item.checked === false){
-            checkboxes[item.value] = false 
-        }
-        })
-        //console.log(Object.entries(checkboxes))
+//obtener valor checkbox
+let checkboxes = {}
+document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+        checkboxes[item.value] = true
+    } else if (item.checked === false) {
+        checkboxes[item.value] = false
+    }
+})
+//console.log(Object.entries(checkboxes))
 
 /* ============ FORM IMG - DRAG AND DROP ============== */
 
@@ -172,106 +155,106 @@ const $img = document.getElementById('img-result')
 $dropZone.addEventListener('click', () => $fileInput.click())
 
 $dropZone.addEventListener('dragover', (e) => {
-	e.preventDefault() //Evita que la imagen se abra en otra pestaña del navegador
+    e.preventDefault() //Evita que la imagen se abra en otra pestaña del navegador
 
-	$dropZone.classList.add('form-file__result--active')
+    $dropZone.classList.add('form-file__result--active')
 })
 
 $dropZone.addEventListener('dragleave', (e) => {
-	e.preventDefault()
+    e.preventDefault()
 
-	$dropZone.classList.remove('form-file__result--active')
+    $dropZone.classList.remove('form-file__result--active')
 })
 
 const uploadImage = (file) => {
-	const fileReader = new FileReader()
-	fileReader.readAsDataURL(file)
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
 
-	fileReader.addEventListener('load', (e) => {
-		$img.setAttribute('src', e.target.result)
-	})
+    fileReader.addEventListener('load', (e) => {
+        $img.setAttribute('src', e.target.result)
+    })
 }
 
 $dropZone.addEventListener('drop', (e) => {
-	e.preventDefault()
+    e.preventDefault()
 
-	/* console.log(e.dataTransfer) */
+    /* console.log(e.dataTransfer) */
 
-	$fileInput.files = e.dataTransfer.files
-	const file = $fileInput.files[0]
+    $fileInput.files = e.dataTransfer.files
+    const file = $fileInput.files[0]
 
-	uploadImage(file)
-	
+    uploadImage(file)
+
 })
 
 $fileInput.addEventListener('change', (e) => {
-	/* console.log(e.target.files[0]) */
-	const file = e.target.files[0]
+    /* console.log(e.target.files[0]) */
+    const file = e.target.files[0]
 
-	uploadImage(file)
+    uploadImage(file)
 })
 
 
 inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
 });
 
 formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
+    e.preventDefault();
 
-	if(campos.nombre && campos.email && campos.telefono ){
-		formulario.reset();
+    if (campos.nombre && campos.email && campos.telefono) {
+        formulario.reset();
         $img.remove();
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 3000);
-		
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
+        document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+        }, 3000);
+
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
 });
 
-/* ================================= BOTON LOGIN ================================= */ 
+/* ================================= BOTON LOGIN ================================= */
 const boton = document.getElementById("abrirModal");
 const modal = document.getElementById("ventanaModal");
 const btnCerrar = document.getElementById("btn-cerrar");
 
-boton.addEventListener ("click", ()=> {
+boton.addEventListener("click", () => {
     modal.showModal();
 });
 
-btnCerrar.addEventListener ("click", ()=> {
+btnCerrar.addEventListener("click", () => {
     modal.close();
 });
 
-/* =============================== VALIDACION LOGIN===================================== */ 
+/* =============================== VALIDACION LOGIN===================================== */
 const userNameField = document.getElementById("uname");
 const passwordField = document.getElementById("psw");
 
 
 const setErrors = (message, field, isError = true) => {
-  if (isError) {
-    field.classList.add("invalid-input");
-    field.nextElementSibling.classList.add("error-input");
-    field.nextElementSibling.innerText = message;
-  } else {
-    field.classList.remove("invalid-input");
-    field.nextElementSibling.classList.remove("error-input");
-    field.nextElementSibling.innerText = "";
-  }
+    if (isError) {
+        field.classList.add("invalid-input");
+        field.nextElementSibling.classList.add("error-input");
+        field.nextElementSibling.innerText = message;
+    } else {
+        field.classList.remove("invalid-input");
+        field.nextElementSibling.classList.remove("error-input");
+        field.nextElementSibling.innerText = "";
+    }
 }
 
 const validateEmptyField = (message, e) => {
-  const field = e.target;
-  const fieldValue = e.target.value;
+    const field = e.target;
+    const fieldValue = e.target.value;
 
-  if (fieldValue.trim().length === 0) {
-    setErrors(message, field);
-  } else {
-    setErrors("", field, false);
-  }
+    if (fieldValue.trim().length === 0) {
+        setErrors(message, field);
+    } else {
+        setErrors("", field, false);
+    }
 }
 
 userNameField.addEventListener("blur", (e) => validateEmptyField("Ingresa tu nombre", e));
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,44 +1,39 @@
 /* ================================ TOGGLE STYLE SWITCHER ======================================*/
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () =>{
+styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 // HIDE STYLE SWITHCER ON SCROLL
 window.addEventListener("scroll", () => {
-    if(document.querySelector(".style-switcher").classList.contains("open"))
-    {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
         document.querySelector(".style-switcher").classList.remove("open");
     }
 })
 /* =============================== THEME COLORS ======================================== */
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color)
-{
+function setActiveStyle(color) {
     alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title"))
-        {
+        if (color === style.getAttribute("title")) {
             style.removeAttribute("disabled");
         }
-        else
-        {
-            style.setAttribute("disabled","True");
+        else {
+            style.setAttribute("disabled", "True");
         }
     }
-    )}
+    )
+}
 /* =============================== MODO OSCURO ======================================== */
 const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () =>{
+dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
 })
-window.addEventListener("load", () =>{
-    if(document.body.classList.contains("dark"))
-    {
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
         dayNight.querySelector("i").classList.add("fa-sun");
     }
-    else
-    {
+    else {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
 })
